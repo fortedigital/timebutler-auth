1e69ff535ca619c5a9241a04435207fb124ad74c
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2023-02-27 11:57:46 +0100

    Sett opp hikari, postgres og flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val webauthn_server_version: String by project
+val hikari_version: String by project
+val flyway_version: String by project
+val postgres_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.10"
@@ -31,6 +34,9 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     compileOnly("com.yubico:webauthn-server-core:$webauthn_server_version")
+    implementation("org.flywaydb:flyway-core:$flyway_version")
+    implementation("org.postgresql:postgresql:$postgres_version")
+    implementation("com.zaxxer:HikariCP:$hikari_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/link/timebutler/Application.kt
+++ src/main/kotlin/link/timebutler/Application.kt
@@ -1,15 +1,45 @@
 package link.timebutler
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.*
+import io.ktor.server.config.*
 import io.ktor.server.netty.*
 import link.timebutler.plugins.configureRouting
 import link.timebutler.plugins.configureSecurity
 import link.timebutler.plugins.configureSerialization
+import org.flywaydb.core.Flyway
 
 fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
+    initDatabase(applicationConfig = environment.config)
     configureSerialization()
     configureSecurity()
     configureRouting()
+}
+
+private fun initDatabase(applicationConfig: ApplicationConfig) {
+    val databaseConfig = HikariConfig().apply {
+        val dbHost = applicationConfig.property("storage.database.host").getString()
+        val dbPort = applicationConfig.property("storage.database.port").getString()
+        val dbName = applicationConfig.property("storage.database.name").getString()
+
+        jdbcUrl = "jdbc:postgresql://$dbHost:$dbPort/$dbName"
+        driverClassName = applicationConfig.property("storage.database.driverClass").getString()
+
+        username = applicationConfig.property("storage.database.user").getString()
+        password = applicationConfig.property("storage.database.password").getString()
+        addDataSourceProperty("databaseName", dbName)
+        addDataSourceProperty("serverName", dbHost)
+        addDataSourceProperty("portNumber", dbPort)
+
+        maximumPoolSize = 10
+        minimumIdle = 1
+        idleTimeout = 100000
+        connectionTimeout = 100000
+        maxLifetime = 300000
+    }
+    val databaseMigrator = Flyway.configure().dataSource(HikariDataSource(databaseConfig)).load()
+    databaseMigrator.migrate()
 }
